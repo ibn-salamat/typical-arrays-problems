36bc8208351ebd80320f8f1bb8240ce84f3f23b1
nurlykhan <n.salamatoff@gmail.com>
2020-09-29 03:03:38 +0600

    feat: finish test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+const handleError = (array) => {
+    if (!Array.isArray(array) || array.length === 0) {
+        return false;
+    } else return true;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    if (handleError(array)) {
+        return array.sort((a, b) => a - b)[0];
+    } else return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (handleError(array)) {
+        return array.sort((a, b) => b - a)[0];
+    } else return 0;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (handleError(array)) {
+        const sum = array.reduce((sum, num) => (sum += num), 0);
+        return sum / array.length;
+    } else return 0;
+};
